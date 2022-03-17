647753782d081e13f5bd5fe91f2c880d79315993
Davi-Ga <davigaldinoky@gmail.com>
2022-03-17 18:08:13 -0300

    Adicionado a tela

--- buscaCEP.py
+++ buscaCEP.py
@@ -1,15 +1,43 @@
+import PySimpleGUI as sg
 import requests
 import json
-import telaInicial
 
-def consultaCep(self,cep):
-    url= requests.get(f'https://viacep.com.br/ws/{cep}/json/')
-    
-    if url.status_code == 200:
-        print("Requisição feita com sucesso")
-    if url.status_code == 400:
-        print("Bad Request 400")
+class TelaInicial:
+    def __init__(self):
+        
+        layout=[
+                 
+                [sg.Text('cep'),sg.Input(size = (25,0), key='CEP')],
+                [sg.Button('Buscar')],
+                [sg.Output(size=(40,10))]
+        
+        ]
+        
+        self.tela=sg.Window('Busca CEP',layout)
+        
+    def consultaCep(self,cep):
+        url= requests.get(f'viacep.com.br/ws/{cep}/json/')
 
-    endereco = url.json()
-    
-    return endereco
+        if url.status_code == 200:
+            print("Requisição feita com sucesso")
+        if url.status_code == 400:
+            print("Bad Request 400")
+
+        endereco = url.json()
+
+        return endereco
+
+    def controllerStart(self):
+        while True:
+            self.button , self.values = self.tela.Read()
+            try:    
+                valores = self.consultacep(self.values['CEP'])
+                for k, v in valores.items():
+                    print(k.upper() , ':' ,v)
+                
+            except:
+                print('Name Error, funcao não definida')
+                
+
+jn = TelaInicial()
+jn.controllerStart()
