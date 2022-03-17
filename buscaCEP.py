01e90c712fcdac762fe8b8bd22fe3242451add48
Davi-Ga <davigaldinoky@gmail.com>
2022-03-17 17:47:44 -0300

    Requisição

--- buscaCEP.py
+++ buscaCEP.py
@@ -3,4 +3,13 @@ import json
 import telaInicial
 
 def consultaCep(self,cep):
-    url= requests.
+    url= requests.get(f'https://viacep.com.br/ws/{cep}/json/')
+    
+    if url.status_code == 200:
+        print("Requisição feita com sucesso")
+    if url.status_code == 400:
+        print("Bad Request 400")
+
+    endereco = url.json()
+    
+    return endereco
